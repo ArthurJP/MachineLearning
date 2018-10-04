3a9d69fc94073cf65c761d27ae4249c8651fcc9b
Arthor <zhangtylord@gmail.com>
2018-10-04 17:08:43 +0900

    feature set

---
+++ test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__arthur__ = "张俊鹏"
+
+LATITUDE_RANGES = zip(range(32, 44), range(33, 45))
+
+for r in LATITUDE_RANGES:
+    print(r)
+    print(r[0])
+    print(r[1])
